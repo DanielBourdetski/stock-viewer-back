c58d9f4666f585b4787f3729ec6d1193bcbe2a6e
DanielBourdetski <Daniel.Bourdetski@gmail.com>
2022-06-02 12:47:49 +0300

    reformated returned res object, cors-enabled server

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const PORT = 3000;
 
 const stocks = require('./routes/stocks');
+
+app.use(cors());
 
 app.use('/', stocks);
 
