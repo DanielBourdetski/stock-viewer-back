e38b1efa7334c0fac6874120c9d9b7b597b8ed80
DanielBourdetski <Daniel.Bourdetski@gmail.com>
2022-06-12 18:58:38 +0300

    created user model

---
+++ model/user.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    immutable: true,
+    default: () => Date.now(),
+  },
+  updatedAt: Date,
+  savedStocks: mongoose.SchemaTypes.ObjectId,
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
