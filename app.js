0776c5199894397d8c4f9624f22cd82ac2cc374e
DanielBourdetski <Daniel.Bourdetski@gmail.com>
2022-06-12 18:57:11 +0300

    reformat

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const PORT = 80;
+const mongoose = require('mongoose');
 
 const stocks = require('./routes/stocks');
+const PORT = 80;
+
+mongoose
+  .connect('mongodb://localhost:27017/myapp')
+  .then(() => console.log('connected to mongoDB'))
+  .catch(err => console.log(err));
 
 app.use(cors());
 
